5952dbdbb02c7035bcc851ad9907835036bc9852
Patrick Zocli <patrickzocli2021@gmail.com>
2024-02-03 21:38:51 +0100

    Fix Avatar component invalid props

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function Avatar({src, size, alt, classname, ...props}: {src: string, size: number, alt: string, [key: string]: any}) {
+export default function Avatar({src, size, alt, className, ...props}: {src: string, size: number, alt: string, [key: string]: any}) {
     return (
         <Image src={src} width={size} height={size} alt={alt} {...props} className={"aspect-square rounded-full " + classname} />
     );
